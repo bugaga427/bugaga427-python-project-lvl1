d9095b573dc1bbcb23e539cfa3927d0d5c2ee742
bugaga427 <vladislav3372@gmail.com>
2020-04-16 22:28:48 +0300

    update prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,14 +7,13 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def round():
     number = randint(2, 100)
     question = str(number)
-
-    divisor = 0
+    choise = 0
 
     for i in range(2, number + 1):
         if number % i == 0:
-            divisor += 1
+            choise += 1
 
-        if divisor == 1:
+        if choise == 1:
             answer = 'yes'
         else:
             answer = 'no'
