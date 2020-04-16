b869bb8a9a4a51521be0b6bfdb5e5c733ac18ca8
bugaga427 <vladislav3372@gmail.com>
2020-04-16 23:50:40 +0300

    fix prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -8,13 +8,13 @@ def round():
     number = randint(2, 100)
     question = str(number)
 
-    choise = 0
+    counter = 0
 
     for i in range(2, number + 1):
         if number % i == 0:
-            choise += 1
+            counter += 1
 
-        if choise == 1:
+        if counter == 1:
             answer = 'yes'
         else:
             answer = 'no'
