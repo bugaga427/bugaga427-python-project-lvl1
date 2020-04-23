7e07b3e044f90d90d463f1466643f7dcbdcc3267
bugaga427 <vladislav3372@gmail.com>
2020-04-23 19:37:04 +0300

    fix brain calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,8 @@
-from brain_games.games import calc
-from brain_games.engine import run
+from brain_games import engine, games
 
 
 def main():
-    run(calc)
+    engine.run(games.calc)
 
 
 if __name__ == '__main__':
