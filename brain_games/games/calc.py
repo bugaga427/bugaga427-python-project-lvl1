6389e05fbbb45588cea07b770b10b4cefe928b74
bugaga427 <vladislav3372@gmail.com>
2020-04-25 23:35:26 +0300

    fix calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,21 +1,17 @@
 from random import randint, choice
-from operator import add, sub, mul
+import operator
 
 
 DESCRIPTION = 'What is the result of the expression?'
+OPERATORS = ((operator.add, '+'), (operator.sub, '-'), (operator.mul, '*'))
 
 
 def new_round():
-    OPERATORS = ((add, '+'), (sub, '-'), (mul, '*'))
-
     first_number = randint(1, 25)
     second_number = randint(1, 25)
     operator, char = choice(OPERATORS)
 
-    if first_number < second_number and char == '-':
-        first_number, second_number = second_number, first_number
-
-    question = '{} {} {}'.format(str(first_number), char, str(second_number))
+    question = '{} {} {}'.format(first_number, char, second_number)
     answer = str(operator(first_number, second_number))
 
     return question, answer
