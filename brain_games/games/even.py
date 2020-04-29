721c1c85c728ff88aaee81025c8fc28dc6c752e8
bugaga427 <vladislav3372@gmail.com>
2020-04-29 11:57:02 +0300

    fix even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,14 +4,10 @@ from random import randint
 DESCRIPTION = 'Answer "yes" if number even otherwise answer "no".'
 
 
-def is_even(number):
-    return number % 2 == 0
-
-
 def new_round():
     question = randint(1, 100)
 
-    if is_even(question):
+    if question % 2 == 0:
         answer = "yes"
     else:
         answer = "no"
